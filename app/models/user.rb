560d3baea764b0dd05eee2bafd7ac24cdfba4e61
Graffias44 <kaochan44.us3126@gmail.com>
2022-04-27 14:03:44 +0900

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates :number,:name, presence: true    # （コードを書き込む）の中身を書き換えてください
-  validates :number,:email, presence: true    # 「（コードを書き込む）の中身を書き換えてください
+  validates :name, presence: true    # （コードを書き込む）の中身を書き換えてください
+  validates :email, presence: true    # 「（コードを書き込む）の中身を書き換えてください
 end
