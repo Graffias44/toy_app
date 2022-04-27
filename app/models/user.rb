4d2f0ff9996d2e3a315e19b16d3748580ccafb80
Graffias44 <kaochan44.us3126@gmail.com>
2022-04-27 12:43:56 +0900

    micro microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :number,:name, presence: true    # （コードを書き込む）の中身を書き換えてください
+  validates :number,:email, presence: true    # 「（コードを書き込む）の中身を書き換えてください
 end
